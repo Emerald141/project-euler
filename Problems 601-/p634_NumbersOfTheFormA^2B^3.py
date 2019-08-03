28f8af1deab28b824d7696d9e1ea63db29dc67b9
Jacob Elafandi <elafa009@umn.edu>
2019-08-02 23:31:55 -0500

    Finishing problem 634: Numbers of the form a^2*b^3 Normally when I finish a problem I feel triumphant, but this one just felt... exhausting? Might just be the time.

--- Problems 601-/p634_NumbersOfTheFormA^2B^3.py
+++ Problems 601-/p634_NumbersOfTheFormA^2B^3.py
@@ -1,12 +1,12 @@
 # Define F(n) to be the number of integers x≤n that can be written in the form
 # x=a^2*b^3, where a and b are integers not necessarily different and both
 # greater than 1.
-# 
+#
 # For example, 32=2^2*2^3 and 72=3^2*2^3 are the only two integers less than
 # 100 that can be written in this form. Hence, F(100)=2.
-# 
+#
 # Further you are given F(2*10^4)=130 and F(3*10^6)=2014.
-# 
+#
 # Find F(9*10^18).
 
 # THEORY:
@@ -31,40 +31,45 @@ def solve(cap = 9 * 10 ** 18):
 	max_b = int((cap/4) ** (1/3))
 	is_prime = [False, False] + [True for b in range(2, max_b + 1)]
 	is_squarefree = [False, False] + [True for b in range(2, max_b + 1)]
-	prime_sixths = []
+	prime_cubes = []
 	result = 0
+	test = set()
 	for b in range(2, max_b + 1):
-		max_a = int(sqrt(cap // (b ** 3)))
 		if is_prime[b]:
+			# Mark all multiples of b as not being prime
 			for mult in range(2 * b, max_b + 1, b):
 				is_prime[mult] = False
+			# Mark all multiples of b^2 as not being squarefree
 			for squaremult in range(b ** 2, max_b + 1, b ** 2):
 				is_squarefree[squaremult] = False
-			result += max_a - 1  # 1 doesn't work
-			# Use inclusion-exclusion to see how many numbers work
-			for length in range(1, len(prime_sixths) + 1):
-				indices = [i for i in range(length)]
-				while True:
-					term = max_a // subproduct(prime_sixths, indices)
-					print(b, max_a, indices, subproduct(prime_sixths, indices), term)
-					result += ((-1) ** length) * term
-					if term != 0 and indices[-1] != len(prime_sixths) - 1:
-						indices[-1] += 1
-						continue
-					for i in range(len(indices) - 2, -1, -1):
-						if indices[i] + 1 != indices[i + 1]:
-							for j in range(len(indices) - 1, i - 1, -1):
-								indices[j] = indices[i] + j - i + 1
+			# Analyze case where the square of b gets plugged in
+			max_a = int(sqrt(cap // (b ** 6)))
+			if max_a >= 2:
+				result += max_a - 1  # 1 doesn't work
+				# Use inclusion-exclusion to see how many numbers work
+				for length in range(1, len(prime_cubes) + 1):
+					indices = [i for i in range(length)]
+					while True:
+						term = max_a // subproduct(prime_cubes, indices)
+						result += ((-1) ** length) * term
+						if term != 0 and indices[-1] != len(prime_cubes) - 1:
+							indices[-1] += 1
+							continue
+						for i in range(len(indices) - 2, -1, -1):
+							if indices[i] + 1 != indices[i + 1]:
+								for j in range(len(indices) - 1, i - 1, -1):
+									indices[j] = indices[i] + j - i + 1
+								break
+						else:
+							# We've found all we can for this length
 							break
-					else:
-						# We've found all we can for this index count
-						break
-			if b ** 6 <= cap:
-				prime_sixths.append(b ** 6)
+				if b ** 3 <= cap:
+					prime_cubes.append(b ** 3)
 		if is_squarefree[b]:
+			max_a = int(sqrt(cap // (b ** 3)))
 			result += max_a - 1
 	return result
 
 if __name__ == "__main__":
 	start = time()
-	peresult(634, solve(3 * 10 ** 6), time() - start)
+	peresult(634, solve(), time() - start)
